a7e3284f5dcae55c7d3f1aba09579c7b6d595704
Bryons-factory <115902848+Bryons-factory@users.noreply.github.com>
2025-04-24 17:53:41 -0500

    Beginning of User Class completed

---
+++ BasicLibraryManagementSystem/LibraryMain/LibraryMain/User.cpp
@@ -0,0 +1,18 @@
+#include "User.h"
+#include <iostream>
+#include <string>
+
+class User {
+private:
+	const char firstName;
+	const char lastName;
+	const char address;
+	const int phoneNum;
+	const char email;
+	const char password;
+	const int phoneNum;
+	const int institutionalID;
+	// Borrowing borrowed;
+
+	//borrowed.getBorrowedBook()
+};
